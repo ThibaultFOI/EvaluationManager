ce2ef4eb2310d6aafd4a4cd9ecb4ff1f5aa904d9
thiba <thiba@>
2023-05-06 01:35:19 +0200

    #4 Create of shelves in UserFormInfo / Create UserDisplayer

--- txtFiletoUI/txtFiletoUI/UserFormInfo.cs
+++ txtFiletoUI/txtFiletoUI/UserFormInfo.cs
@@ -8,9 +8,23 @@ namespace txtFiletoUI
 {
     internal class UserFormInfo
     {
+        string Name;
+        string Surname;
+        string YearofBirth;
+        string City;
+        string Faculty;
+        string Role;
+        string specificattribute;
 
         public UserFormInfo(string[] param)
         {
+            Name = param[0];
+            Surname = param[1];
+            YearofBirth = param[2];
+            City = param[3];
+            Faculty = param[4];
+            Role = param[5];
+            specificattribute = "mystery";
 
         }
     }
